0fe9f6203b6ea03c298ade132db6242e544a72eb
Rich <36368842+Riicha@users.noreply.github.com>
2018-06-27 22:30:10 -0400

    upload

--- config.py
+++ config.py
@@ -1,9 +1,4 @@
-
-# coding: utf-8
-
-# In[ ]:
-
-
-# Create a settings object with your API key and preferred units
-api_key = "4af76c79df8d11f6295bc8323877e2e5"
-
+consumer_key = 'b2jDtC9vAdTpnuJDZkNPkwEYF'
+consumer_secret = 'xEo1L3eRSaE6J7uOIlYP0ohoNp0jikv3LKunjp9arE0YWn5tVo'
+access_token = '57237295-K2EPPUBgXZfz83NuA92RxkzETWThD4YW8lt3bhAcW'
+access_token_secret = '9GD3xt89mMZNCKWmDwQvnaWw3tQjaz0vgz4l4d90Fb7N0'
